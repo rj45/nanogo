1f951dac23f1c82a1d2b262f14611dcc88c6be16
Ryan Sanche <ryan@rj45.ca>
2021-11-07 06:23:14 -0800

    Beginnings of code generation for go compiler

--- gorj.go
+++ gorj.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/rj45/nanogo/codegen"
 	"github.com/rj45/nanogo/parser"
 )
 
@@ -13,4 +15,8 @@ func main() {
 	mod := parser.ParseModule("./testfiles/seive/seive.go")
 
 	fmt.Println(mod.LongString())
+
+	fmt.Print("\n\n--------------------\n\n")
+
+	codegen.GenerateCode(mod, os.Stdout)
 }
